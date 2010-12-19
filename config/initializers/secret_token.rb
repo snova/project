47686edc30d8be0d1f90d2808dc3d00727cde6bc
nova <nova@nova-laptop.(none)>
2010-12-19 00:20:26 -0500

    Commit 1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curious::Application.config.secret_token = 'db81ef30ba4cecfab9377888d048458ccd227d96825c73cd479e32769c95fe5ca91a722bdaeb425c49b8f85ca38c3ec92ff1ca425a0ee3325de19ba60ce23d5a'
